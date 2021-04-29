eea56e1a11c0de2789bd4156a2dd282629b46015
milica p <ndksn@gmail.com>
2021-04-29 21:30:24 +0200

    j

--- o/p/main.c
+++ o/p/main.c
@@ -1,19 +1,6 @@
-#include <stdio.h>
+
 
 void PrintResult(int res)
 {
 printf("Result: %d\n", res);
-}
-
-int main()
-{
-int a = 9;
-int b = 3;
-int c;
-if(a > b)
-c = a - b;
-else
-c = b - a;
-printf("Result: %d\n", c);
-return 0;
 }
